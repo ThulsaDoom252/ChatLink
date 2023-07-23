aacfcd006bdcd630b30b01290dd48d0b2e1dfc92
ThulsaDoom252 <xenolm252@gmail.com>
2023-07-24 00:56:23 +0300

    Added UserBox and UserList components

--- app/users/components/UserList.tsx
+++ app/users/components/UserList.tsx
@@ -1,12 +1,41 @@
+import {User} from "@prisma/client";
+import UserBox from "@/app/users/components/UserBox";
+
 interface UserListProps {
-    items: []
+    items: User[]
 }
 
 const UserList: React.FC<UserListProps> = ({items}) => {
     return (
-        <div>
-            Users list
-        </div>
+        <aside className="
+        fixed
+        inset-y-0
+        pl-20
+        lg: pb-20
+        lg:w-80
+        lg:block
+        overflow-y-auto
+        border-r
+        border-gray-200
+        block
+        w-full
+        left-0
+        ">
+            <div className='px-5'>
+                <div className='flex-col'>
+                    <div className="
+                    text-2xl
+                    font-bold
+                    text-neutral-800
+                    py-4
+                    ">
+                        Users
+                    </div>
+
+                </div>
+                {items.map(item => <UserBox key = {item.id} data = {item}/>)}
+            </div>
+        </aside>
     );
 };
 
